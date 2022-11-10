dd884b87e4c89d27db244b7766729ea5ca547882
SIOUkoeran <alstnalsgud@gmail.com>
2022-11-10 22:50:49 +0900

    #2 init editing-server
    협업 편집 서버 초기 설정

---
+++ editing-server/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "editing-server"
---
+++ editing-server/src/test/kotlin/com/example/editingserver/EditingServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.editingserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EditingServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
